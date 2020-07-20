0690df5078bb31d90b0236919c14de3477841c3f
chenjb <chenjb04@163.com>
2020-07-20 11:18:38 +0800

    'python'

--- 作用域.py
+++
@@ -1,4 +0,0 @@
-"""
-当 Python 遇到一个变量的话他会按照这样的顺序进行搜索：
-本地作用域（Local）→当前作用域被嵌入的本地作用域（Enclosing locals）→全局/模块作用域（Global）→内置作用域（Built-in）
-"""
--- 自身.py
+++
@@ -1,9 +0,0 @@
-"""
-运行时获取对像的类型
-"""
-a = [1]	
-print(type(a))  # <class 'list'>
-print(isinstance(a, list))  # True
-print(dir(a))  # 获取a拥有的方法
-print(hasattr(list, '1'))  # False
-print(getattr(list, 'pop')) # <method 'pop' of 'list' objects>
